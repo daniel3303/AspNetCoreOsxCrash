e2e8a3bc98d24cf9b863926aef3b2c2fc25df438
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2024-11-25 16:44:16 +0000

    Initial commit

---
+++ Web/App/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Intranet.Web.Portal.Controllers;
+
+public class HomeController : Controller {
+    public IActionResult Index() {
+        return View();
+    }
+}
